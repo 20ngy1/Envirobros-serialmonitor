55739131d823b7b2e461ae28318795fd8f9931af
20ngy1 <74828869+20ngy1@users.noreply.github.com>
2020-11-22 00:10:50 +0800

    Add files via upload

--- Hackathon_code.ino
+++ Hackathon_code.ino
@@ -1,3 +1,5 @@
+//Enviro-bros - display water conductivity and temperature 
+
 int conductivityReading; //initialize a variable for the sensor reading
 int tempReading;       
   
@@ -8,14 +10,13 @@ void setup() {
   Serial.begin(9600); // set up the serical communiction to print the reading
 }
 
-
 void loop() {
   conductivityReading = analogRead(A0); //get the analog reading of the sensor
   tempReading = analogRead(A1);
 
-  float voltage = conductivityReading*(5.0/1023.0)-1.70-0.24-0.3; //put the data in terms of voltage used(5 volts)
-  float temperature = ((tempReading)-273-32)*5/9-19;
+  float voltage = conductivityReading*(5.0/1023.0)-2.32;    //put the data in terms of voltage used(5 volts)
+  float temperature = ((tempReading)-273-32)*5/9-19;                 //convert temperature to celcius and calibrate
 
-  Serial.println("Voltage:   " + String(voltage)+"         " + "Temperature:   " + String(temperature)); //print the current reading to the serial part 
-  
+  Serial.println("Voltage:   " + String(voltage)+"         " + "Temperature:   " + String(temperature)); //print the voltage and temp reading to the serial monitor
+
 }
