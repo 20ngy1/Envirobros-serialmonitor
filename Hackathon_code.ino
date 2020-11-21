0101a12025b3f1e2c49225a50585787564d755f5
20ngy1 <74828869+20ngy1@users.noreply.github.com>
2020-11-22 00:04:33 +0800

    Add files via upload

---
+++ Hackathon_code.ino
@@ -0,0 +1,21 @@
+int conductivityReading; //initialize a variable for the sensor reading
+int tempReading;       
+  
+const int BTState = 2; //bluetooth module state pin 2
+
+void setup() {
+  pinMode(BTState, INPUT);
+  Serial.begin(9600); // set up the serical communiction to print the reading
+}
+
+
+void loop() {
+  conductivityReading = analogRead(A0); //get the analog reading of the sensor
+  tempReading = analogRead(A1);
+
+  float voltage = conductivityReading*(5.0/1023.0)-1.70-0.24-0.3; //put the data in terms of voltage used(5 volts)
+  float temperature = ((tempReading)-273-32)*5/9-19;
+
+  Serial.println("Voltage:   " + String(voltage)+"         " + "Temperature:   " + String(temperature)); //print the current reading to the serial part 
+  
+}
